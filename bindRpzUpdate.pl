ba31a4e1c34c4431c4a14f95f56b95c27ec721b1
biscuitNinja <cookie@biscuit.ninja>
2016-10-16 12:15:45 +0100

    Block card skimming domains
    Now includes domains featuring card skimming malware as idenitifed by Willem de Groot
    https://gitlab.com/gwillem/public-snippets/snippets/28813

--- bindRpzUpdate.pl
+++ bindRpzUpdate.pl
@@ -24,6 +24,9 @@ m/^#/));
   push (@in_rpz, $_);
 }   
 
+#
+# advertising servers
+#
 my @content = split /\n/, get
 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=one-line&showintro=0&startdate[day]=&startdate[month]=&startdate[year]=';
 die "Couldn't get adserver list" unless @content;
@@ -44,6 +47,26 @@ foreach my $dn (split /,/, $main::dns) {
   printf $out "%-46sCNAME   %s\n", $dn, CNAME;
   $main::rpzUpdated = 1;
 }
+
+#
+# card skimming domains
+#
+@content = split /\n/, get
+'https://gitlab.com/gwillem/public-snippets/snippets/28813';
+die "Couldn't get card skimming domains list" unless @content;
+
+# Parse response data
+DN:
+foreach my $line (@content) {
+  next unless ($line =~ s/(<pre class="code highlight"><code>)?<span id="LC\d+" class="line">(.*)<\/span>(<\/code><\/pre>)?/$2/e);
+  $main::cardSkimmingSitesParsed = 1;
+  foreach (@in_rpz) {
+    next DN if ($_ =~ m/^$line/);
+  }
+  printf $out "%-45s CNAME   %s\n", $line, CNAME;
+  $main::rpzUpdated = 1;
+}
+die "Failed to parse card skimming domains list" unless $main::cardSkimmingSitesParsed;
 
 close $out;
 
